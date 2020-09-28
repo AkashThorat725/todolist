ba9fdfcd330247a5f3260fb58f2c184a96229a21
AkashThorat725 <akashst1999@gmail.com>
2020-09-28 13:00:23 +0530

    adding icons

--- components/heading/heading.js
+++ components/heading/heading.js
@@ -3,6 +3,13 @@ export class Heading extends Component {
     render (){
         return (
             <div>
+                <nav className="navbar navbar-light bg-light">
+  <form className="form-inline">
+    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
+</nav>
+              
                 <h1>Notes</h1>
             </div>
         );
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { Heading } from "../components/heading/heading";
-import { Notes } from "../notes/notes";
+import { Notes } from "../components/heading/notes/notes";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
